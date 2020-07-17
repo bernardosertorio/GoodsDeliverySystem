11bb6fbc55a50bd29ab42c6aa2c0977aed0f7dff
Bernardo Sertório <besertorio@gmail.com>
2020-07-17 20:41:02 -0300

    :art: improving the structure

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -2,10 +2,20 @@ const Sequelize = require("sequelize")
 const dbConfig = require("../config/database")
 const User = require("../app/models/User")
 
+const models = [User]
 
-const connection = new Sequelize(dbConfig)
+class Connection {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.connection = new Sequelize(dbConfig)
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models))
+  }
+}
 
 
-User.init(connection)
-
-module.exports = connection
+module.exports = new Connection
