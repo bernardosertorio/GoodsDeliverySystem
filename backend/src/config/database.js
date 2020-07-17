c0454e5041ecd3960daf9062a187278ecae1f5a3
Bernardo Sertório <besertorio@gmail.com>
2020-07-17 14:17:28 -0300

    :rocket:Database implementation

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,10 @@
-require("dotenv/config")
 
 module.exports = {
   dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  username: "bernardosertorio",
+  password: "",
+  database: "deliveryusers",
   define: {
     timestamps: true,
     underscored: true,
