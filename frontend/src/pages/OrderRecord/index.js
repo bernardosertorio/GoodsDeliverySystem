3c9562a501ef03509e177d8095872de5d3c35635
Bernardo Sertório <besertorio@gmail.com>
2020-07-19 14:31:33 -0300

    :construction: Implementing database registration

--- frontend/src/pages/OrderRecord/index.js
+++ frontend/src/pages/OrderRecord/index.js
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'
+import { toast } from 'react-toastify'
+import * as Yup from 'yup'
+import DateConfig from '../../components/DateConfig'
+import PropTypes from 'prop-types'
+
+import api from '../../services/api'
 
 import './styles.css'
 
+const schema = Yup.object().shape({
+  name: Yup.string().required(),
+  date: Yup.date().required(),
+  starting_journey: Yup.string().required(),
+  ending_journey: Yup.string().required(),
+});
 
-export default function OrderRecord() {
+
+export default function OrderRecord({history}) {
+
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
+
+  async function handleRegister(e, data) {
+    e.preventDefault()
+
+    try {
+      await api.post('/', {
+        name: data.name,
+        date: data.date,
+        starting_journey: start,
+        ending_journey: end,
+      })
+      history.pushState('/listrecords')
+    } catch (err) {
+     toast.error(
+       'Registration error! Check your data.'
+     )
+     console.log(err) 
+    }
+  }
+
   return (
     <div className="orderRecord-container">
       <section className="form">
         <h1> Goods Delivery System </h1>
 
-        <form>
+        <form onSubmit={handleRegister} schema={schema}>
           <h2>Delivery Record</h2>
 
-          <input placeholder="Client's name" />
-          <input placeholder="Delivery date" />
+          <input type="text" name="name" placeholder="Client's name" />
+          <input placeholder="Delivery date"><DateConfig name="date" /></input>
           <input placeholder="Starting point" />
           <input placeholder="Arrival point" />
 
