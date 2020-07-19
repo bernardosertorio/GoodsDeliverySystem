39f069c25177d9d9e3bbf29cf061f33b41065203
Bernardo Sertório <besertorio@gmail.com>
2020-07-18 21:35:38 -0300

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Header from './Header'
+import './global.css'
+import Routes from './routes'
 
 
 function App() {
   return (
-    <Header title="GoodsDeliverySystem"/>
+    <Routes />
   );
 }
 
