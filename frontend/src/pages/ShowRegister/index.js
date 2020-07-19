cf532bb17be37f40a4199df7924999672542a071
Bernardo Sertório <besertorio@gmail.com>
2020-07-19 00:05:52 -0300

    :construction: implementing and styling

---
+++ frontend/src/pages/ShowRegister/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { withScriptjs, withGoogleMap } from 'react-google-maps'
+
+const WrapperMap = withScriptjs(withGoogleMap(Map))
+
+export default function MapContainer() {
+  return (
+    <div style={{width: `100vw`, height:`100vh`}}>
+      <WrapperMap
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API}`}
+      loadingElement={<div style={{ height: `100%` }} />}
+      containerElement={<div style={{ height: `100%` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+    />
+    </div>
+  )
+}
