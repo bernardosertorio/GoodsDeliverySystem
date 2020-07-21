87a0445757fd5f720bc11638f08111313b91f166
Bernardo Sertório <besertorio@gmail.com>
2020-07-20 23:12:30 -0300

    :construction: implementing googlemap and setting

---
+++ frontend/src/components/GoogleMap/index.js
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react'
+import { withRouter } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Geocode from 'react-geocode'
+import { GoogleMap, DirectionsRenderer } from 'react-google-maps'
+import MapStyles from './styles'
+
+import api from '../../services/api'
+
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API)
+
+function GoogleMaps({ match }) {
+
+  const [ways, setWays] = useState('')
+
+  async function loadWays() {
+
+    const apiResponse = await api.get(`/register/${match.params.id}`)
+
+    const { starting_journey, ending_journey } = apiResponse.data
+
+    const startPoint = await Geocode.fromAddress(starting_journey)
+    const { lat, lng } = startPoint.results[0].geometry.location 
+
+    const endPoint = await Geocode.fromAddress(ending_journey)
+    const { lat: latitude, lng: longitude } = endPoint.results[0].geometry.location 
+
+    const waysService = new window.google.maps.DirectionsService()
+
+    const beginningJourney = { lat, lng}
+    const endingTrip = { lat: latitude, lng: longitude}
+
+    waysService.route({
+
+      beginningJourney,
+      endingTrip,
+      travelMode: window.google.maps.TravelMode.DRIVING
+
+    },
+
+    (result, status) => {
+
+      if(status === window.google.maps.DirectionsStatus.OK) {
+        setWays(result)
+        console.log(result)
+
+      } else {
+
+        console.error(`error fetching directions ${result}`)
+
+      }
+      }
+    )
+  }
+
+  loadWays = useCallback(
+    () => {
+      ()
+    },[]
+  ) 
+
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: -22.9035, lng: -43.2096 }}
+      defaultOptions={{ styles: MapStyles }}
+    >
+      <DirectionsRenderer ways={ways} />
+    </GoogleMap>
+  )
+}
+
+GoogleMaps.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string
+    })
+  }).isRequired
+}
+
+export default withRouter(GoogleMaps)
+
+
+
---
+++ frontend/src/components/PlacesComplete/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PlacesAutoComplete from 'react-places-autocomplete'
+import { Input } from '@rocketseat/unform'
+import { Container } from './styles'
+import PropTypes from 'prop-types'
+
+export default function PlacesComplete({
+  onSelect,
+  onChange,
+  value,
+  placeholder,
+  name
+}) {
+  PlacesComplete.defaultProps ={
+    placeholder: "Campo de input",
+    name: 'autocomplete'
+  }
+
+  PlacesComplete.propTypes = {
+    onSelect: PropTypes.func.isRequired,
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    name: PropTypes.string, 
+  }
+
+  const searchOptions = {
+    componentRestrictions: { country : ['br'] },
+    types: [ 'adress' ] 
+  }
+
+  return (
+
+    <PlacesAutoComplete
+    searchOptions={searchOptions} 
+    value={value}
+    onChange={onChange}
+    onSelect={onSelect}
+    placeholder={placeholder}
+  >
+    {({ getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+      <>
+        <Input name={name} type="text" {...getInputProps({ placeholder })} />
+
+        <div>
+          {loading ? <div>...Loading</div> : null}
+
+          {suggestions.map(suggestion => {
+            const className = suggestion.active
+            ? 'suggestion-item-active'
+            : 'suggestion-item'
+
+            return (
+              <Container
+                {...getSuggestionItemProps(suggestion, { className })}
+              >
+                <div className={className}>{suggestion.description}</div>
+              </Container>
+            )  
+          })}
+        </div>
+      </>
+    )}
+  </PlacesAutoComplete>
+  )
+}
