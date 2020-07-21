41c1a72af11f1b7c8e1aec2e920c6d45c83b2f22
Bernardo Sertório <besertorio@gmail.com>
2020-07-20 23:08:20 -0300

    :construction: connecting with backend and fix

--- frontend/src/pages/OrderRecord/index.js
+++ frontend/src/pages/OrderRecord/index.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
+import { Form, Input } from '@rocketseat/unform'
 import { FiLogIn } from 'react-icons/fi'
 import { toast } from 'react-toastify'
 import * as Yup from 'yup'
-import DateConfig from '../../components/DateConfig'
 import PropTypes from 'prop-types'
+import 'react-datepicker/dist/react-datepicker.css'
+import PlacesComplete from '../../components/PlacesComplete'
+import DateConfig from '../../components/DateConfig'
 
 import api from '../../services/api'
 
@@ -18,7 +21,7 @@ const schema = Yup.object().shape({
 });
 
 
-export default function OrderRecord({history}) {
+function OrderRecord({history}) {
 
   const [start, setStart] = useState('')
   const [end, setEnd] = useState('')
@@ -33,7 +36,7 @@ export default function OrderRecord({history}) {
         starting_journey: start,
         ending_journey: end,
       })
-      history.pushState('/listrecords')
+      history.push('/listrecords')
     } catch (err) {
      toast.error(
        'Registration error! Check your data.'
@@ -47,24 +50,43 @@ export default function OrderRecord({history}) {
       <section className="form">
         <h1> Goods Delivery System </h1>
 
-        <form onSubmit={handleRegister} schema={schema}>
+        <Form onSubmit={handleRegister} schema={schema}>
           <h2>Delivery Record</h2>
 
-          <input type="text" name="name" placeholder="Client's name" />
-          <input placeholder="Delivery date"><DateConfig name="date" /></input>
-          <input placeholder="Starting point" />
-          <input placeholder="Arrival point" />
+          <Input type="text" name="name" placeholder="Client's name" />
+          <DateConfig name="date" />
+          <PlacesComplete
+            onChange={setStart}
+            value={start}
+            onSelect={data => setStart(data)}
+            name={start}
+            placeholder="Starting point" 
+          />
+          <PlacesComplete
+            onChange={setEnd}
+            value={end}
+            onSelect={data => setEnd(data)}
+            name={end}
+            placeholder="Arrival point" 
+          />
 
-          <Link to="/listrecords" className="button" type="submit">Register Delivery</Link>
+          <button className="button" type="submit">Register Delivery</button> 
 
           <Link to="/listrecords">
             <FiLogIn size={16} color="#E02041" />
             List of delivery orders
           </Link>
-
-        </form>
+        </Form>
       </section>
-
     </div>
   )
 }
+
+  OrderRecord.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func,
+    }).isRequired,  
+  }
+
+  export default withRouter(OrderRecord)
+
