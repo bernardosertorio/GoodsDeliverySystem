a15f384e7735d246130b31dadabd7c5a9293de71
Bernardo Sertório <besertorio@gmail.com>
2020-07-20 23:10:58 -0300

    :construction: implementing googlemap and setting

--- frontend/src/pages/ShowRegister/index.js
+++ frontend/src/pages/ShowRegister/index.js
@@ -3,15 +3,15 @@ import { withScriptjs, withGoogleMap } from 'react-google-maps'
 
 const WrapperMap = withScriptjs(withGoogleMap(Map))
 
-export default function MapContainer() {
+export default function UserMapWay() {
   return (
     <div style={{width: `100vw`, height:`100vh`}}>
       <WrapperMap
-      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API}`}
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?=${process.env.REACT_APP_GOOGLE_API}`}
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `100%` }} />}
       mapElement={<div style={{ height: `100%` }} />}
-    />
+      />
     </div>
   )
 }
